f37a6269977d088cb88311470f8602e26d16cfed
Erik C. Thauvin <erik@thauvin.net>
2024-08-28 18:47:25 -0700

    Revert antlr4 update to preserve backward compatibility

--- src/bld/java/rife/bld/extension/Antlr4Build.java
+++ src/bld/java/rife/bld/extension/Antlr4Build.java
@@ -30,7 +30,7 @@ public class Antlr4Build extends Project {
         repositories = List.of(MAVEN_CENTRAL, RIFE2_RELEASES);
         scope(compile)
             .include(dependency("com.uwyn.rife2", "bld", version(2,0,1)))
-            .include(dependency("org.antlr", "antlr4", version(4,13,2)));
+            .include(dependency("org.antlr", "antlr4", version(4,11,1)));
         scope(test)
             .include(dependency("org.junit.jupiter", "junit-jupiter", version(5,11,0)))
             .include(dependency("org.junit.platform", "junit-platform-console-standalone", version(1,11,0)));
