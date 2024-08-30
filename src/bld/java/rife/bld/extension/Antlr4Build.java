47f3280a5e42be6f572b5e9bf8e728b3ccbc45b7
Geert Bevin <gbevin@uwyn.com>
2024-08-29 22:09:41 -0400

    Updated to bld 2.1.0. Upgraded version to 1.4.0.

--- src/bld/java/rife/bld/extension/Antlr4Build.java
+++ src/bld/java/rife/bld/extension/Antlr4Build.java
@@ -20,7 +20,7 @@ public class Antlr4Build extends Project {
     public Antlr4Build() {
         pkg = "rife.bld.extension";
         name = "Antlr4";
-        version = version(1,3,0);
+        version = version(1,4,0);
         archiveBaseName = "bld-antlr4";
 
         javaRelease = 17;
@@ -29,7 +29,7 @@ public class Antlr4Build extends Project {
 
         repositories = List.of(MAVEN_CENTRAL, RIFE2_RELEASES);
         scope(compile)
-            .include(dependency("com.uwyn.rife2", "bld", version(2,0,1)))
+            .include(dependency("com.uwyn.rife2", "bld", version(2,1,0)))
             .include(dependency("org.antlr", "antlr4", version(4,11,1)));
         scope(test)
             .include(dependency("org.junit.jupiter", "junit-jupiter", version(5,11,0)))
